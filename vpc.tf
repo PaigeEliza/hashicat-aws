dca622b7c9d65693d143e8a20ce523275edeed31
Paige <paige3perry@gmail.com>
2020-06-10 18:31:50 +0000

    added vpc

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,5 @@
 module "vpc" {
-  source  = "app.terraform.io/PAIGE-training/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "2.39.0"
+  # insert the 12 required variables here
 }
